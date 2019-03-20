b59ca4eb3064eaeeee77fb7b7e8815dd0d25a91b
Emiyaaaaa <2914034404@qq.com>
2019-03-20 19:37:02 +0800

    Update bilibili_intro_command_of_1818hjy.js

--- bilibili_intro_command_of_1818hjy.js
+++ bilibili_intro_command_of_1818hjy.js
@@ -1,8 +1,11 @@
 (function() {
-    if ($('#viewbox_report .video-title .tit').html().indexOf('1818黄金眼')>-1){
-    	console.log($(".bilibili-player-video video"))
-    	video_slider = document.getElementsByClassName('bilibili-player-video-progress-slider');
-    	a = document.getElementById('playerWrap')
-    	console.log(a.offsetTop)
+    if ($('#viewbox_report .video-title .tit').html().indexOf('1818黄金眼') > -1){
+    	video = $(".bilibili-player-video video")[0]
+    	video_slider = document.getElementsByClassName('bilibili-player-video-progress-slider')["0"];
+    	video_sendbar = document.getElementsByClassName('bilibili-player-video-sendbar')[0];
+    	video_control = document.getElementsByClassName('bilibili-player-video-control-wrap');
+    	player_wrap = document.getElementById('playerWrap');
+    	console.log(player_wrap.offsetTop,video.clientHeight,video_sendbar.clientHeight,$(video_control).height(),video_slider)
+    	setTimeout(function(){console.log(document.getElementsByClassName('bilibili-player-video-control-wrap')[0].clientHeight)},2000)
     }
 })();
