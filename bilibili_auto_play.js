fdb4298bdb55ae14c87a15c53402b54bb1dcf98a
Emiyaaaaa <2914034404@qq.com>
2019-03-30 19:48:18 +0800

    Update bilibili_auto_play.js

--- bilibili_auto_play.js
+++ bilibili_auto_play.js
@@ -4,7 +4,6 @@
     	function waitLoading(){
             setTimeout(function(){
                 var video_time = document.getElementsByClassName('bilibili-player-video-time-total')[0];
-                console.log(video_time)
                 if (video_time == undefined) {
                     waitLoading();
                 }
@@ -13,19 +12,39 @@
                 }
                 else{
                     var video_play_button = document.getElementsByClassName('bilibili-player-video-state')[0];
-                    var video_volume_button = document.getElementsByClassName('bilibili-player-video-btn-volume')[0];
                     $(video_play_button).click();
-                    $(video_volume_button).click()
-                    console.log(video_volume_button)
+                    var play_time = document.getElementsByClassName('bilibili-player-video-time-now')[0];
+                    play_time_now = time2sec(play_time.innerText);
+                    get_time_now();
+
                 }
             },50);
         }
         waitLoading();
     }
+
     function time2sec(time){
         min = time.split(':')[0];
         sec = time.split(':')[1];
         all_sec = Number(min)*60 + Number(sec);
         return all_sec;
+    }
+
+    function get_time_now(){
+        setTimeout(function(){
+            var play_time = document.getElementsByClassName('bilibili-player-video-time-now')[0];
+            play_time_now = time2sec(play_time.innerText);
+            surplus_time = time2sec(video_time.innerText) - play_time_now - 3;
+            if (surplus_time >= 0) {
+                open_new_video();
+            }
+            else{
+                get_time_now();
+            }
+            
+        },200);
+    }
+    function open_new_video(){
+        
     }
 })();
