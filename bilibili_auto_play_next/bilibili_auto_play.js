544245025e73790a9adbf19f9021ffcc87090d6d
Emiyaaaaa <2914034404@qq.com>
2019-04-08 19:09:02 +0800

    Update bilibili_auto_play.js

--- bilibili_auto_play_next/bilibili_auto_play.js
+++ bilibili_auto_play_next/bilibili_auto_play.js
@@ -1,12 +1,11 @@
 (function() {
+    now_av = window.location.href.split('/')[4];
+    now_av_id = now_av.slice(2,)
     if ($('#viewbox_report .video-title .tit').html().indexOf('1818黄金眼') > -1){
-        now_av = window.location.href.split('/')[4];
-        now_av_id = now_av.slice(2,)
-        
+        not_1818 = false;
     	function waitLoading(){
             setTimeout(function(){
                 video_time = document.getElementsByClassName('bilibili-player-video-time-total')[0];
-                console.log(video_time)
                 if (video_time == undefined) {
                     waitLoading();
                 }
@@ -21,15 +20,45 @@
                     var play_time = document.getElementsByClassName('bilibili-player-video-time-now')[0];
                     play_time_now = time2sec(play_time.innerText);
                     get_time_now();
-
                 }
             },50);
         }
         waitLoading();
     }
     else{
-        var video_play_button = document.getElementsByClassName('bilibili-player-video-state')[0];
-        $(video_play_button).click();
+        not_1818 = true;
+        function waitLoading(){
+            setTimeout(function(){
+                video_time = document.getElementsByClassName('bilibili-player-video-time-total')[0];
+                if (video_time == undefined) {
+                    waitLoading();
+                }
+                else if (video_time.innerText == '00:00') {
+                    waitLoading();
+                }
+                else{
+                    var video_play_button = document.getElementsByClassName('bilibili-player-video-state')[0];
+                    $(video_play_button).click();
+
+                    var p_video_ele = document.getElementById('multi_page');
+                    if (p_video_ele != undefined) {
+                        setTimeout(function(){
+                            p_video_list = p_video_ele.getElementsByClassName('list-box')[0].getElementsByTagName('li');
+                            for (var i = 0; i < p_video_list.length; i++) {
+                                if (p_video_list[i].classList.contains('on')) {
+                                    now_av_p = i + 1;
+                                    break;
+                                }
+                            }
+                        },2500);
+                    }
+                    var play_time = document.getElementsByClassName('bilibili-player-video-time-now')[0];
+                    play_time_now = time2sec(play_time.innerText);
+                    get_time_now();
+                }
+            },50);
+        }
+        waitLoading();
     }
 
     function time2sec(time){
@@ -44,8 +73,16 @@
             var play_time = document.getElementsByClassName('bilibili-player-video-time-now')[0];
             play_time_now = time2sec(play_time.innerText);
             surplus_time = time2sec(video_time.innerText) - play_time_now - 2;
+            if (not_1818) {
+                surplus_time = time2sec(video_time.innerText) - play_time_now - 1;
+            }
             if (surplus_time <= 0) {
-                open_new_video();
+                if (not_1818) {
+                    open_new_p_video();
+                }
+                else{
+                    open_new_video();
+                }
             }
             else{
                 get_time_now();
@@ -57,6 +94,11 @@
         var now_av_index = all_video.indexOf(now_av_id);
         if (now_av_index != 0) {
             window.location.href = 'https://www.bilibili.com/video/av' + all_video[now_av_index-1];
+        }
+    }
+    function open_new_p_video(){
+        if (now_av_p < p_video_list.length) {
+            window.location.href = 'https://www.bilibili.com/video/av' + now_av_id + '/?p=' + (now_av_p + 1);
         }
     }
 })();
