b407a0e55afdee54b2e202b9fc31889de677c8ad
Emiyaaaaa <2914034404@qq.com>
2019-05-09 13:45:43 +0800

    Update collection_1818hjy_av2.js

--- bilibili_auto_play_next/collection_1818hjy_av2.js
+++ bilibili_auto_play_next/collection_1818hjy_av2.js
@@ -1,5 +1,7 @@
 (function () {
 	page = 2;
+	txt = 'all_video = new Array(';
+
 	function waitLoading(){
 		setTimeout(function(){
 			var all_video = document.querySelector('.section .clearfix .small-item');
@@ -7,44 +9,44 @@
 				waitLoading();
 			}
 			else{
-				var txt = 'all_video = new Array(';
-				if (page == 1) {
-					getVideoAv(document);
-				}
-				else if (page > 1) {
-					for (var i = 2; i <= page; i++) {
-						$.ajax({
-							url:'https://space.bilibili.com/354077280/video/',
-							type:'GET',
-		        			data:{"page":String(i)},
-							success:function(data){
-								console.log(data)
-							}
-						})
-						getVideoAv();
+				all_video = document.querySelectorAll('.section .clearfix .small-item');
+				console.log(all_video)
+
+				for (var i = 0; i < all_video.length; i++) {
+					av_id = $(all_video[i]).attr('data-aid');
+					av_title = $(all_video[i].querySelectorAll('a')[1]).attr('title');
+					if (av_title.indexOf('1818黄金眼') == -1) {
+						continue;
 					}
+					txt += "'" + av_id + "'" + ",";
+					// console.log(txt)
 				}
-				// downloadTextFile(txt);
+				document.getElementsByClassName('be-pager-next')[0].click();
 			}
 		},50);
-	}
-	waitLoading();
-
-	function getVideoAv(document){
-		all_video = document.querySelectorAll('.section .clearfix .small-item');
-		for (var i = 0; i < all_video.length; i++) {
-			av_id = $(all_video[i]).attr('data-aid');
-			av_title = $(all_video[i].querySelectorAll('a')[1]).attr('title');
-			if (av_title.indexOf('1818黄金眼') == -1) {
-				continue;
-			}
-			txt += "'" + av_id + "'" + ",";
-		}
-		return txt;
 	}
 
 	function downloadTextFile(mobileCode) {
 	    var file = new File([mobileCode], "avid.js", {type: "text/plain;charset=utf-8"});
 	    saveAs(file);
 	}
+
+	function waitLoading2(){
+		setTimeout(function(){
+			var next_page = document.getElementsByClassName('be-pager-next')[0];
+			if (next_page == null) {
+				waitLoading2();
+			}
+			else{
+				for (var i = 0; i < page; i++) {
+					waitLoading();
+					if((i + 1) != page){
+						console.log($(next_page).click());
+						$(next_page).click();
+					}
+				}
+			}
+		},50);
+	}
+	waitLoading2();
 })();
