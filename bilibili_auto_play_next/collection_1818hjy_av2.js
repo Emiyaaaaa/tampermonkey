67875b6ef0f4cd3c0d6fd63288702609e168bdc0
Emiyaaaaa <2914034404@qq.com>
2019-05-09 13:08:35 +0800

    Update collection_1818hjy_av2.js

--- bilibili_auto_play_next/collection_1818hjy_av2.js
+++ bilibili_auto_play_next/collection_1818hjy_av2.js
@@ -1,5 +1,5 @@
 (function () {
-	page = 1;
+	page = 2;
 	function waitLoading(){
 		setTimeout(function(){
 			var all_video = document.querySelector('.section .clearfix .small-item');
@@ -8,24 +8,41 @@
 			}
 			else{
 				var txt = 'all_video = new Array(';
-				all_video = document.querySelectorAll('.section .clearfix .small-item');
-				for (var i = 0; i < all_video.length; i++) {
-					av_id = $(all_video[i]).attr('data-aid');
-					av_title = $(all_video[i].querySelectorAll('a')[1]).attr('title');
-					if (av_title.indexOf('1818黄金眼') == -1) {
-						continue;
-					}
-					txt += "'" + av_id + "'" + ",";
+				if (page == 1) {
+					getVideoAv(document);
 				}
-				txt = txt.slice(0,txt.length - 1) + ');';
-				a = document.getElementsByClassName('be-pager-item')
-				console.log(a)
-				// $(a[4]).click()
+				else if (page > 1) {
+					for (var i = 2; i <= page; i++) {
+						$.ajax({
+							url:'https://space.bilibili.com/354077280/video/',
+							type:'GET',
+		        			data:{"page":String(i)},
+							success:function(data){
+								console.log(data)
+							}
+						})
+						getVideoAv();
+					}
+				}
 				// downloadTextFile(txt);
 			}
 		},50);
 	}
 	waitLoading();
+
+	function getVideoAv(document){
+		all_video = document.querySelectorAll('.section .clearfix .small-item');
+		for (var i = 0; i < all_video.length; i++) {
+			av_id = $(all_video[i]).attr('data-aid');
+			av_title = $(all_video[i].querySelectorAll('a')[1]).attr('title');
+			if (av_title.indexOf('1818黄金眼') == -1) {
+				continue;
+			}
+			txt += "'" + av_id + "'" + ",";
+		}
+		return txt;
+	}
+
 	function downloadTextFile(mobileCode) {
 	    var file = new File([mobileCode], "avid.js", {type: "text/plain;charset=utf-8"});
 	    saveAs(file);
