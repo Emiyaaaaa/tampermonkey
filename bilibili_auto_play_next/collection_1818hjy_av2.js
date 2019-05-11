6a1fe2d4db15480c43c9a0febbe907b99e939da8
Emiyaaaaa <2914034404@qq.com>
2019-05-11 15:40:15 +0800

    Update collection_1818hjy_av2.js

--- bilibili_auto_play_next/collection_1818hjy_av2.js
+++ bilibili_auto_play_next/collection_1818hjy_av2.js
@@ -1,41 +1,60 @@
 (function () {
-	page = 2;
-	console.log('a')
+	page = 3;
 	txt = 'all_video = new Array(';
+	// now_page = window.location.search[window.location.search.indexOf('page=')+'page='.length];
+	// function ToFirstPage(){
+	// 	setTimeout(function(){
+	// 		var first_page = document.getElementsByClassName('be-pager-item')[0];
+	// 		if (first_page == null) {
+	// 			ToFirstPage();
+	// 		}
+	// 		else{
+	// 			first_page.click();
+	// 			setTimeout(function(){waitLoading();}, 500)
 
-	// for (var i = 0; i < page; i++) {
+	// 		}
+	// 	},50);
+	// }
+	// if(now_page != undefined && now_page != 1) {
+	// 	ToFirstPage();
+	// }
+	// else{
 	// 	waitLoading();
 	// }
-	waitLoading()
+
 	function waitLoading(){
 		setTimeout(function(){
-			var all_video = document.querySelector('.section .clearfix .small-item');
+			var all_video = document.querySelector('.section .clearfix .small-item a');
 			if (all_video == null) {
 				waitLoading();
 			}
 			else{
-				all_video = document.querySelectorAll('.section .clearfix .small-item');
-				console.log(all_video)
-
+				all_video = document.getElementsByClassName('small-item');
 				for (var i = 0; i < all_video.length; i++) {
 					av_id = $(all_video[i]).attr('data-aid');
 					av_title = $(all_video[i].querySelectorAll('a')[1]).attr('title');
 					if (av_title.indexOf('1818黄金眼') == -1) {
 						continue;
 					}
-					txt += "'" + av_id + "'" + ",";
-					// console.log(txt)
+					txt += "'" + av_id + "',";
 				}
-				document.getElementsByClassName('be-pager-next')[0].click();
-				now_page = window.location.search[window.location.search.indexOf('page=')+'page='.length];
-				console.log(now_page)
-
-				if (now_page <= page) {
-					waitLoading();
-				}
+				
+				setTimeout(function(){
+					document.getElementsByClassName('be-pager-next')[0].click();
+					now_page = window.location.search[window.location.search.indexOf('page=')+'page='.length];
+					if (now_page <= page){
+						setTimeout(function(){waitLoading();},500);
+					}
+					else{
+						txt = txt.slice(0,txt.length - 1) + ');';
+						console.log(txt);
+						downloadTextFile(txt);
+					}
+				},800);
 			}
 		},50);
 	}
+	waitLoading();
 
 	function downloadTextFile(mobileCode) {
 	    var file = new File([mobileCode], "avid.js", {type: "text/plain;charset=utf-8"});
