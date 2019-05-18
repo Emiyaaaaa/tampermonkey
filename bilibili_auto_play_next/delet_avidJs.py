2f93545a38a1dbb390f877be0a3b244d198a71d9
Emiyaaaaa <2914034404@qq.com>
2019-05-18 14:03:35 +0800

    update

--- bilibili_auto_play_next/delet_avidJs.py
+++ bilibili_auto_play_next/delet_avidJs.py
@@ -1,9 +1,16 @@
 import os
-my_file = 'E:/chrome下载/avid.js' # 文件路径
-if os.path.exists(my_file):
-    os.remove(my_file)
 for i in range(1,11):
-    my_file = 'E:/chrome下载/avid ('+ str(i) +').js'
-    print(my_file)
-    if os.path.exists(my_file):
-        os.remove(my_file)
+    avid_files = 'E:/chrome下载/avid ('+ str(i) +').js'
+    if not os.path.exists(avid_files):
+        break
+
+if i != 1:
+    avid_file = 'E:/chrome下载/avid.js'
+    if os.path.exists(avid_file):
+        os.remove(avid_file)
+    os.rename('E:/chrome下载/avid ('+ str(i-1) +').js','E:/chrome下载/avid.js')
+
+for i in range(1,11):
+    avid_files = 'E:/chrome下载/avid ('+ str(i) +').js'
+    if os.path.exists(avid_files):
+        os.remove(avid_files)
